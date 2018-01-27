d0b2b9108ddd9d41c772a137b495aed56afff01e
Frank <sunwu51@126.com>
2018-01-28 02:41:54 +0800

    first commit

---
+++ bootstrap.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+arr=$(ls -F|grep '/$')
+
+trans(){
+  echo $1..........
+  cd $1
+  mdfiles=$(ls|grep 'md$')
+  htfiles=$(ls|grep 'html$')
+  for j in $mdfiles
+  do
+    tmp=${j%.*}".html"
+    if [[ "${htfiles[@]}" =~ $tmp ]]
+    then
+      echo "include ${j%.*}.html"
+    else
+      echo "shangwei trans ${j%.*}.md"
+      ghmd ${j%.*}.md
+    fi
+  done
+  cd ..
+}
+
+
+
+for i in $arr
+do
+  trans $i
+done
