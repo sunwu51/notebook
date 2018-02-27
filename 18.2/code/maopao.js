e3b9842f2aa5c3e2d0063d3ac2b3517e8b5d6d49
Frank <sunwu51@126.com>
2018-02-28 01:02:16 +0800

    update sort

--- 18.2/code/maopao.js
+++ 18.2/code/maopao.js
@@ -1,4 +1,4 @@
-function swap(arr,i,j){
+function swap(arr, i, j) {
     let t = arr[i]
     arr[i] = arr[j]
     arr[j] = t
@@ -7,14 +7,14 @@ function swap(arr,i,j){
 /**
  *  1  冒泡排序 
  */
-var arr = [9,1,5,8,3,7,6,4,2]
-var flag = true 
-for(let i=0;i<arr.length && flag;i++){
+var arr = [9, 1, 5, 8, 3, 7, 6, 4, 2]
+var flag = true
+for (let i = 0; i < arr.length && flag; i++) {
     //从后往前，相邻比较，最小的浮到最前面
-    flag= false //优化，如果已经是结果了，则提前停止冒泡
-    for(let j=arr.length-1;j>=i;j--){
-        if(arr[j-1]>arr[j]){
-            swap(arr,j,j-1);
+    flag = false //优化，如果已经是结果了，则提前停止冒泡
+    for (let j = arr.length - 1; j >= i; j--) {
+        if (arr[j - 1] > arr[j]) {
+            swap(arr, j, j - 1);
             flag = true
         }
     }
@@ -23,13 +23,13 @@ console.log(arr)
 /**
  * 2 选择排序
  */
-var arr = [9,1,5,8,3,7,6,4,2]
-for(let i=0;i<arr.length;i++){
-    for(let j=i+1;j<arr.length;j++){
-        if(arr[j]<arr[i]){
+var arr = [9, 1, 5, 8, 3, 7, 6, 4, 2]
+for (let i = 0; i < arr.length; i++) {
+    for (let j = i + 1; j < arr.length; j++) {
+        if (arr[j] < arr[i]) {
             let t = arr[i]
-            arr[i]=arr[j]
-            arr[j]=t
+            arr[i] = arr[j]
+            arr[j] = t
         }
     }
 }
@@ -37,33 +37,70 @@ console.log(arr)
 /**
  * 3 插入排序
  */
-var arr = [9,1,5,8,3,7,6,4,2]
-var tmp,j
-for(let i=1; i<arr.length; i++){
-    if(arr[i]<arr[i-1]){
+var arr = [9, 1, 5, 8, 3, 7, 6, 4, 2]
+var tmp, j
+for (let i = 1; i < arr.length; i++) {
+    if (arr[i] < arr[i - 1]) {
         tmp = arr[i]
-        for(j=i-1; arr[j]>tmp;j--){
-            arr[j+1]=arr[j]
+        for (j = i - 1; arr[j] > tmp; j--) {
+            arr[j + 1] = arr[j]
         }
-        arr[j+1]=tmp
+        arr[j + 1] = tmp
     }
 }
+console.log(arr)
 /**
  * 4 希尔排序
  */
-var arr = [0,9,1,5,8,3,7,6,4,2]
-var i,j,tmp
-let increment=arr.length
-do{
-    increment=parseInt(increment/3)+1
-    for(i=increment+1;i<arr.length;i++){
-        if(arr[i]<arr[i-increment])
-            arr[0]=arr[i]
-            for(j=i-increment;j>0 && arr[0]<arr[j];j-=increment){
-                arr[j+increment]=arr[j]
-            }
-            arr[j+increment]=arr[0]
+var arr = [ 9, 1, 5, 8, 3, 7, 6, 4, 2]
+var gap, i, j;
+var temp;
+for (gap = arr.length >> 1; gap > 0; gap >>= 1){
+    for (i = gap; i < arr.length; i++) {
+        temp = arr[i];
+        for (j = i - gap; j >= 0 && arr[j] > temp; j -= gap)
+            arr[j + gap] = arr[j];
+        arr[j + gap] = temp;
     }
 }
-while(increment>1);
 console.log(arr)
+/**
+ * 5 堆排序
+ */
+/**
+ * 6 归并排序
+ */
+var arr = [ 9, 1, 5, 8, 3, 7, 6, 4, 2]
+Array.prototype.merge_sort = function() {
+	var merge = function(left, right) {
+		var final = [];
+		while (left.length && right.length)
+			final.push(left[0] <= right[0] ? left.shift() : right.shift());
+		return final.concat(left.concat(right));//当left或right中一个长度为0的时候，直接连接起来
+	};
+	var len = this.length;
+	if (len < 2) return this;
+	var mid = len / 2;
+	return merge(this.slice(0, parseInt(mid)).merge_sort(), this.slice(parseInt(mid)).merge_sort());
+};
+console.log(arr.merge_sort())
+/**
+ * 7 快速排序
+ */
+Array.prototype.quick_sort = function() {
+	var len = this.length;
+	if (len <= 1)
+		return this.slice(0);
+	var left = [];
+	var right = [];
+	var mid = [this[0]];
+	for (var i = 1; i < len; i++)
+		if (this[i] < mid[0])
+			left.push(this[i]);
+		else
+			right.push(this[i]);
+	return left.quick_sort().concat(mid.concat(right.quick_sort()));
+};
+
+var arr = [5, 3, 7, 4, 1, 9, 8, 6, 2];
+arr = arr.quick_sort();
--- 18.2/utils_排序.md
+++ 18.2/utils_排序.md
@@ -27,18 +27,107 @@ for(int i=0;i<arr.length;i++){
 ```
 # 3 插入排序
 一个有序的序列，插入一个新值时，先和最后一个比较，如果大则直接放最后，如果小则和倒数第二个比，依次类推。是建立在一种原本是有序，后面插入新值的角度思考的。
-```
-//arr[0]是哨兵 实际排序没有这个值 有的算法中额外申请一个变量作为哨兵
-for(int i>2; i<arr.length; i++){
-    if(arr[i]<arr[i-1]){
-        arr[0] = arr[i]
-        for(j=i-1; arr[j]>arr[0];j--){
-            arr[j+1]=arr[j]
+```javascript
+var tmp, j
+for (let i = 1; i < arr.length; i++) {
+    if (arr[i] < arr[i - 1]) {
+        tmp = arr[i]
+        for (j = i - 1; arr[j] > tmp; j--) {
+            arr[j + 1] = arr[j]
         }
-        arr[j+1]=arr[0]
+        arr[j + 1] = tmp
     }
 }
 ```
 # 4 希尔排序
+思想为：首先将数组从中间分开，然后对应位置比较，将较小的放在前半（此时步长设为了len/2）。然后将步长缩短如变为len/4，然后还是对应位置比较，通过插入排序将越小的放在越前面。直到步长缩短为1，此时即等于插入排序。该算法通过前期跳跃性的移动节省了插入排序的交换次数，获的了比插入排序这种简单排序更好的复杂度。这个[视频](https://www.youtube.com/watch?v=CmPA7zE8mx0)可以快速风趣的了解希尔排序。
+```javascript
+var arr = [ 9, 1, 5, 8, 3, 7, 6, 4, 2]
+var gap, i, j; //gap步长
+var temp;
+for (gap = arr.length >> 1; gap > 0; gap >>= 1){//步长一开始是数组长度/2，每次缩小一半
+    for (i = gap; i < arr.length; i++) {//将第一组外的数进行遍历
+        temp = arr[i];
+        //下面三行代码：每一列同一位置的元素进行比较.将较小的放在前面.插入排序，只不过步长是gap
+        //可以对比上面的插入排序，比较异同
+        for (j = i - gap; j >= 0 && arr[j] > temp; j -= gap)
+            arr[j + gap] = arr[j];
+        arr[j + gap] = temp;
+    }
+}
+```
 # 5 堆排序
 利用存储结构堆，堆是一种完全二叉树，每个节点的孩子都比自己小，这就是大顶堆，反之小顶堆。用大顶堆存储数据，然后逐个弹出根节点，就是从大到小的顺序排列。
+```javascript
+function swap(i, j) {
+	var tmp = arr[i];
+	arr[i] = arr[j];
+	arr[j] = tmp;
+}
+
+function max_heapify(start, end) {
+	//建立父節點指標和子節點指標
+	var dad = start;
+	var son = dad * 2 + 1;
+	if (son >= end)//若子節點指標超過範圍直接跳出函數
+		return;
+	if (son + 1 < end && arr[son] < arr[son + 1])//先比較兩個子節點大小，選擇最大的
+		son++;
+	if (arr[dad] <= arr[son]) {//如果父節點小於子節點時，交換父子內容再繼續子節點和孫節點比較
+		swap(dad, son);
+		max_heapify(son, end);
+	}
+}
+
+var len = arr.length;
+//初始化，i從最後一個父節點開始調整
+for (var i = Math.floor(len / 2) - 1; i >= 0; i--)
+	max_heapify(i, len);
+//先將第一個元素和已排好元素前一位做交換，再從新調整，直到排序完畢
+for (var i = len - 1; i > 0; i--) {
+	swap(0, i);
+	max_heapify(0, i);
+}
+```
+# 归并排序
+分治思想，将数组中相邻俩数组成一组，一共len/2组，每组俩数排序。然后将相邻两组合并为一组，因为组内已经排好序了，所以合并的时候有些简化的操作（从a和b组中依次拿出最小的，看谁的更小，作为新组的小值，然后次小...）
+```javascript
+Array.prototype.merge_sort = function() {
+	var merge = function(left, right) {
+		var final = [];
+		while (left.length && right.length)
+			final.push(left[0] <= right[0] ? left.shift() : right.shift());
+		return final.concat(left.concat(right));//当left或right中一个长度为0的时候，直接连接起来
+	};
+	var len = this.length;
+	if (len < 2) return this;
+	var mid = len / 2;
+	return merge(this.slice(0, parseInt(mid)).merge_sort(), this.slice(parseInt(mid)).merge_sort());
+};
+```
+# 快速排序
+同样利用分治的思想，将第一个数取出来作为mid，然后后面的和他比较，大于他的归为一组，其他归为一组。然后第一组一定是所有元素排在第二组所有元素后面的。将第这两组进行同样的操作，一直到最后每组大小为1.
+```javascript
+Array.prototype.quick_sort = function() {
+	var len = this.length;
+	if (len <= 1)
+		return this.slice(0);
+	var left = [];
+	var right = [];
+	var mid = [this[0]];
+	for (var i = 1; i < len; i++)
+		if (this[i] < mid[0])
+			left.push(this[i]);
+		else
+			right.push(this[i]);
+	return left.quick_sort().concat(mid.concat(right.quick_sort()));
+};
+```
+# 复杂度问题
+这里时间复杂度只讨论均值。  
+前三种为简单排序，平均时间复杂度都是O(n2)【n的平方】。shell排序在步长不同的情况下不同  
+![image](img/shell.jpg)  
+堆排序时间复杂度`O(nlogn)`，空间复杂度`O(1)`  
+归并时间复杂度`O(nlogn)`,空间`O(n)`  
+快速时间复杂度`O(nlogn)`,空间`O(n)`优化后空间复杂度更低  
+
