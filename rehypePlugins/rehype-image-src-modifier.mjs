10affc0f4074c95b8746511cdbf65493ed57f17e
Frank <sunwu51@126.com>
2024-08-18 09:35:32 +0000

    imgur->github

--- rehypePlugins/rehype-image-src-modifier.mjs
+++ rehypePlugins/rehype-image-src-modifier.mjs
@@ -38,7 +38,8 @@ function rehypeImageSrcModifier() {
                 }
                 // 如果本地启动无法翻墙，本地build请注释掉下面imgur这段代码
                 else if (process.env.NODE_ENV === 'production' && src.startsWith('https://i.imgur.com/')) {
-                    let picName = src.replace('https://i.imgur.com/', '/api/imgur?filename=');
+                    // let picName = src.replace('https://i.imgur.com/', '/api/imgur?filename=');
+                    let picName = src.replace('https://i.imgur.com/', 'https://raw.githubusercontent.com/sunwu51/notebook/gh-pages/');
                     node.properties.src = picName;
                 }
             }
